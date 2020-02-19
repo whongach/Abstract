aa66ed91de931f30f9d168777e5397d0980fc1c7
Kyle James <kcj7751@ad.rit.edu>
2020-02-19 09:16:27 -0500

    Add name to line #8 in Game1

--- GroupGame/Game1.cs
+++ GroupGame/Game1.cs
@@ -5,7 +5,7 @@ using Microsoft.Xna.Framework.Input;
 namespace GroupGame
 {
     /// <summary>
-    /// This is the main type for your game.
+    /// Kyle was here.
     /// </summary>
     public class Game1 : Game
     {
