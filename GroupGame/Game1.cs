93e18dbcc44a5eb3bffe995f2e352bcb01310ddf
dwj2733 <dwj2733@rit.edu>
2020-02-19 09:18:11 -0500

    added name to comment on line 8

--- GroupGame/Game1.cs
+++ GroupGame/Game1.cs
@@ -5,7 +5,7 @@ using Microsoft.Xna.Framework.Input;
 namespace GroupGame
 {
     /// <summary>
-    /// This is the main type for your game.
+    /// my name is Dean Jones 
     /// </summary>
     public class Game1 : Game
     {
