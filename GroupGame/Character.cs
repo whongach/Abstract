b0e70613349dcd3945160e5e0fc0716974b7b8c4
Dean Jones <deanjones128@gmail.com>
2020-02-26 01:05:24 -0500

    Added basic implementations of all classes and interfaces

---
+++ GroupGame/Character.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GroupGame
+{
+    class Character : GameObject , ICollidable
+    {
+        int health;
+        Weapon weapon;
+        Rectangle hitbox;
+        bool circleBox;
+
+        public Rectangle Hitbox
+        {
+            get { return hitbox; }
+        }
+
+        public bool CircleBox
+        {
+            get { return circleBox; }
+        }
+    }
+}
---
+++ GroupGame/Player.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GroupGame
+{
+    class Player : Character
+    {
+        Weapon offHand;
+        Item currentItem;
+    }
+}
