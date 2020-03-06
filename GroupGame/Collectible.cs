f1b5a7ac89b40d86fc146ac82e4f1c7da4e40ef4
BuildTools <unconfigured@null.spigotmc.org>
2020-03-05 20:13:35 -0500

    Re-organize, clean, and comment the Collectible abstract class

--- GroupGame/Collectible.cs
+++ GroupGame/Collectible.cs
@@ -1,47 +1,55 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
+/// <summary>
+/// The namespace for the Game.
+/// </summary>
 namespace GroupGame
 {
+    /// <summary>
+    /// Abstract class for Collectible items.
+    /// </summary>
     abstract class Collectible : GameObject, ICollidable
     {
-        //fields
-        bool circleBox;
-        bool pickedUp;
+        // Fields
+        private bool circleBox;
+        private bool pickedUp;
 
-        //properties 
-        public bool CircleBox
-        {
-            get { return circleBox; }
-        }
+        // Properties 
+        /// <summary>
+        /// Gets whether the Collectible is a Circle or a Box.
+        /// </summary>
+        public bool CircleBox { get { return circleBox; } }
 
-        public bool PickedUp
-        {
-            get { return pickedUp; }
-            set { pickedUp = value; }
-        }
+        /// <summary>
+        /// Gets and sets whether the Collectible has been Picked Up or not.
+        /// </summary>
+        public bool PickedUp { get { return pickedUp; } set { pickedUp = value; } }
 
-        //constructor
+        // Constructors
+        /// <summary>
+        /// Constructs a Collectible GameObject that is Collidable.
+        /// </summary>
+        /// <param name="position">The Collectible's position.</param>
+        /// <param name="sprite">The Collectible's sprite.</param>
+        /// <param name="circular">Whether or not the Collectible's shape is circular or rectangular.</param>
+        /// <param name="equipped">Where or not the Collectible is equipped.</param>
         public Collectible(Rectangle position, Texture2D sprite, bool circular, bool equipped) : base(position, sprite)
         {
+            // Initialize fields
             circleBox = circular;
             pickedUp = equipped;
         }
 
-        //methods
-
+        // Methods
         /// <summary>
-        /// draw method fro items in inventory
+        /// Draw method for items in the inventory.
         /// </summary>
-        /// <param name="sb">spritebatch used to draw item</param>
-        /// <param name="displayPos">the position of the inventory box</param>
+        /// <param name="sb">A SpriteBatch object.</param>
+        /// <param name="displayPos">The position to draw the inventory box.</param>
         public virtual void Draw(SpriteBatch sb, Rectangle displayPos)
         {
+            // Draw the collectible
             sb.Draw(sprite, displayPos, Color.White);
         }
     }
