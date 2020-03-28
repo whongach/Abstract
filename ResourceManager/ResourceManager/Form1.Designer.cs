303f57d93db7453cf487de4da7edb80c49854d41
Dean Jones <deanjones128@gmail.com>
2020-03-28 14:59:09 -0400

    created a resource manager that currently only works for maps

---
+++ ResourceManager/ResourceManager/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace ResourceManager
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.addMap = new System.Windows.Forms.Button();
+            this.addEnemy = new System.Windows.Forms.Button();
+            this.addWeapon = new System.Windows.Forms.Button();
+            this.resetData = new System.Windows.Forms.Button();
+            this.deleteButton = new System.Windows.Forms.Button();
+            this.viewItems = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // addMap
+            // 
+            this.addMap.Location = new System.Drawing.Point(12, 12);
+            this.addMap.Name = "addMap";
+            this.addMap.Size = new System.Drawing.Size(123, 88);
+            this.addMap.TabIndex = 0;
+            this.addMap.Text = "Add Map";
+            this.addMap.UseVisualStyleBackColor = true;
+            this.addMap.Click += new System.EventHandler(this.addMap_Click);
+            // 
+            // addEnemy
+            // 
+            this.addEnemy.Location = new System.Drawing.Point(146, 12);
+            this.addEnemy.Name = "addEnemy";
+            this.addEnemy.Size = new System.Drawing.Size(123, 88);
+            this.addEnemy.TabIndex = 1;
+            this.addEnemy.Text = "Add Enemy";
+            this.addEnemy.UseVisualStyleBackColor = true;
+            this.addEnemy.Click += new System.EventHandler(this.addEnemy_Click);
+            // 
+            // addWeapon
+            // 
+            this.addWeapon.Location = new System.Drawing.Point(279, 12);
+            this.addWeapon.Name = "addWeapon";
+            this.addWeapon.Size = new System.Drawing.Size(123, 88);
+            this.addWeapon.TabIndex = 2;
+            this.addWeapon.Text = "Add Weapon";
+            this.addWeapon.UseVisualStyleBackColor = true;
+            this.addWeapon.Click += new System.EventHandler(this.addWeapon_Click);
+            // 
+            // resetData
+            // 
+            this.resetData.Location = new System.Drawing.Point(12, 107);
+            this.resetData.Name = "resetData";
+            this.resetData.Size = new System.Drawing.Size(123, 23);
+            this.resetData.TabIndex = 3;
+            this.resetData.Text = "Reset";
+            this.resetData.UseVisualStyleBackColor = true;
+            this.resetData.Click += new System.EventHandler(this.resetData_Click);
+            // 
+            // deleteButton
+            // 
+            this.deleteButton.Location = new System.Drawing.Point(146, 106);
+            this.deleteButton.Name = "deleteButton";
+            this.deleteButton.Size = new System.Drawing.Size(123, 23);
+            this.deleteButton.TabIndex = 4;
+            this.deleteButton.Text = "Delete Item";
+            this.deleteButton.UseVisualStyleBackColor = true;
+            this.deleteButton.Click += new System.EventHandler(this.deleteButton_Click);
+            // 
+            // viewItems
+            // 
+            this.viewItems.Location = new System.Drawing.Point(279, 106);
+            this.viewItems.Name = "viewItems";
+            this.viewItems.Size = new System.Drawing.Size(123, 23);
+            this.viewItems.TabIndex = 5;
+            this.viewItems.Text = "View Items";
+            this.viewItems.UseVisualStyleBackColor = true;
+            this.viewItems.Click += new System.EventHandler(this.viewItems_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(414, 142);
+            this.Controls.Add(this.viewItems);
+            this.Controls.Add(this.deleteButton);
+            this.Controls.Add(this.resetData);
+            this.Controls.Add(this.addWeapon);
+            this.Controls.Add(this.addEnemy);
+            this.Controls.Add(this.addMap);
+            this.Name = "Form1";
+            this.Text = "ResourceManager";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button addMap;
+        private System.Windows.Forms.Button addEnemy;
+        private System.Windows.Forms.Button addWeapon;
+        private System.Windows.Forms.Button resetData;
+        private System.Windows.Forms.Button deleteButton;
+        private System.Windows.Forms.Button viewItems;
+    }
+}
+
---
+++ ResourceManager/ResourceManager/Form1.cs
@@ -0,0 +1,218 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+//ResourceManager is a class that allows the user to add maps, weapons and enemies to the resource file that the game loads on launch
+namespace ResourceManager
+{
+    public partial class Form1 : Form
+    {
+        //fields
+        private BinaryReader reader;
+        private Stream mainFile;
+        private Stream dataFile;
+        private BinaryWriter writer;
+        private OpenFileDialog open;
+        private OpenFileDialog data;
+
+
+        //constructor
+        //additionally opens the file to be saved to
+        public Form1()
+        {
+            InitializeComponent();
+            do
+            {
+                open = new OpenFileDialog();
+                open.Title = "Open Resource File";
+                open.Filter = "Resource Files|*.rsrc";
+            } while (open.ShowDialog() != DialogResult.OK);
+        }
+        /// <summary>
+        /// opens the necessary file and initializes the writer
+        /// </summary>
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        /// <summary>
+        /// opens a dialogue allowing the user to add maps to the file
+        /// </summary>
+        private void addMap_Click(object sender, EventArgs e)
+        {
+            mainFile = new FileStream(open.FileName, FileMode.Append, FileAccess.Write);
+            writer = new BinaryWriter(mainFile);
+            data = new OpenFileDialog();
+            data.Title = "Open Map File";
+            data.Filter = "Map Files|*.map";
+            if (data.ShowDialog() == DialogResult.OK)
+            {
+                dataFile = File.OpenRead(data.FileName);
+                reader = new BinaryReader(dataFile);
+                writer.Write("Map");
+                writer.Write(data.FileName);
+                for (int i = 0; i < 256; i++)
+                    writer.Write(reader.ReadInt32());
+                dataFile.Close();
+                MessageBox.Show("Map Added Successfully");
+            }
+            writer.Close();
+        }
+
+        /// <summary>
+        /// opens a dialogue allowing the user to add enemies to the file
+        /// </summary>
+        private void addEnemy_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        /// <summary>
+        /// opens a dialogue allowing the user to add weapons to the file
+        /// </summary>
+        private void addWeapon_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        /// <summary>
+        /// deletes all data from the resource file
+        /// </summary>
+        private void resetData_Click(object sender, EventArgs e)
+        {
+            File.Delete(open.FileName);
+            mainFile = File.OpenWrite(open.FileName);
+            writer = new BinaryWriter(mainFile);
+            writer.Close();
+        }
+
+        /// <summary>
+        /// deletes a selected item from the resource file
+        /// </summary>
+        private void deleteButton_Click(object sender, EventArgs e)
+        {
+            //determines the line that the data to be deleted is one
+            int line = 0;
+            int type = 0;
+            data = new OpenFileDialog();
+            data.Title = "Open Data File";
+            mainFile = File.OpenRead(open.FileName);
+            reader = new BinaryReader(mainFile);
+            if (data.ShowDialog() == DialogResult.OK)
+            {
+                while (reader.PeekChar() != -1)
+                {
+                    switch (reader.ReadString())
+                    {
+                        case "Map":
+                            if(data.FileName == reader.ReadString())
+                            {
+                                MessageBox.Show("File Located");
+                                type = 1;
+                                break;
+                            }
+                            for (int i = 0; i < 256; i++)
+                            {
+                                reader.ReadInt32();
+                                line++;
+                            }
+                            break;
+                    }
+                    if (type != 0)
+                        break;
+                    line++;
+                }
+            }
+            reader.Close();
+            //rewrites the file, skipping the data to be deleted
+            mainFile = File.OpenRead(open.FileName);
+            reader = new BinaryReader(mainFile);
+            dataFile = File.OpenWrite("temp.bin");
+            writer = new BinaryWriter(dataFile);
+            int currentLine = 0;
+            //adds all data before the data to be deleted
+            while (currentLine < line)
+            {
+                switch (reader.ReadString())
+                {
+                    case "Map":
+                        writer.Write("Map");
+                        writer.Write(reader.ReadString());
+                        for (int i = 0; i < 256; i++)
+                        {
+                            writer.Write(reader.ReadInt32());
+                        }
+                        currentLine += 258;
+                        break;
+                }
+            }
+            //skips deleted data
+            switch (type)
+            {
+                case 1:
+                    reader.ReadString();
+                    reader.ReadString();
+                    for(int i = 0; i<256; i++)
+                    {
+                        reader.ReadInt32();
+                    }
+                    break;
+            }
+            //adds data after deleted data
+            while (reader.PeekChar() != -1)
+            {
+                switch (reader.ReadString())
+                {
+                    case "Map":
+                        writer.Write("Map");
+                        writer.Write(reader.ReadString());
+                        for (int i = 0; i < 256; i++)
+                        {
+                            writer.Write(reader.ReadInt32());
+                        }
+                        break;
+                }
+            }
+            writer.Close();
+            reader.Close();
+            File.Replace("temp.bin", open.FileName, "resBack.bin");
+        }
+
+        /// <summary>
+        /// shows all items in a message box
+        /// </summary>
+        private void viewItems_Click(object sender, EventArgs e)
+        {
+            List<string> items = new List<string>();
+            mainFile = File.OpenRead(open.FileName);
+            reader = new BinaryReader(mainFile);
+            string messageText = "";
+            while (reader.PeekChar() != -1)
+            {
+                switch (reader.ReadString())
+                {
+                    case "Map":
+                        items.Add(reader.ReadString());
+                        for (int i = 0; i < 256; i++)
+                            reader.ReadInt32();
+                        break;
+                }
+            }
+            for(int i = 0; i<items.Count; i++)
+            {
+                items[i] = items[i].Split('\\')[items[i].Split('\\').Length - 1];
+                messageText += items[i] + "\n";
+            }
+            MessageBox.Show(messageText);
+            reader.Close();
+        }
+    }
+}
